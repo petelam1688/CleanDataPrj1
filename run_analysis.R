1c7862fe80b55fa8d7ae44b39b5180b3edd0c8cd
petelam1688 <petelam@yahoo.com>
2015-04-25 16:23:25 -0700

    v2

--- run_analysis.R
+++ run_analysis.R
@@ -2,11 +2,16 @@ require("data.table")
 require("reshape2")
 
 ## load raw data into data table
-tblTraingData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/prj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
-tblTraingDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/prj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
-tblTraingDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/prj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
-tblTestData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/prj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
-tblTestDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/prj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
-tblTestDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/prj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
+tblTrainingData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
+tblTraininggDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+tblTraininggDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
+tblTestData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
+tblTestDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+tblTestDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
 
+tblFeatures <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/features.txt")
 ##
+
+## rename column names
+names(tblTrainingData) <- "ID"
+names(tblTestData) <- "ID"
