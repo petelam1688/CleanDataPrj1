30ed4291ec8d6f5b1143c4733461de9d0dfa2520
petelam1688 <petelam@yahoo.com>
2015-04-25 18:16:44 -0700

    modify path with assumption of local file path

--- run_analysis.R
+++ run_analysis.R
@@ -2,14 +2,14 @@ require("data.table")
 require("reshape2")
 
 ## load raw data into data table
-tblTrainingData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
-tblTrainingDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
-tblTrainingDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
-tblTestData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
-tblTestDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
-tblTestDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
+tblTrainingData <- read.table("subject_train.txt")
+tblTrainingDataX <- read.table("X_train.txt")
+tblTrainingDataY <- read.table("Y_train.txt")
+tblTestData <- read.table("subject_test.txt")
+tblTestDataX <- read.table("X_test.txt")
+tblTestDataY <- read.table("Y_test.txt")
 
-tblFeatures <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/features.txt")
+tblFeatures <- read.table("eatures.txt")
 ##
 
 ## rename column names
@@ -47,4 +47,4 @@ tblDataFinal <- dcast(tblDataFilteredCollapseReadings, ID+Activity ~ variable, m
 
 
 ## extract data into txt format
-write.csv(tblDataFinal, "E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/tblDataFinal.csv", row.names=FALSE)
+write.table(tblDataFinal, "tblDataFinal.txt", row.name=FALSE)
