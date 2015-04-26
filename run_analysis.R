715055901884f40464644da1ac951b76ce479764
petelam1688 <petelam@yahoo.com>
2015-04-25 17:15:06 -0700

    complete combinging all data

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,8 @@ require("reshape2")
 
 ## load raw data into data table
 tblTrainingData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
-tblTraininggDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
-tblTraininggDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
+tblTrainingDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+tblTrainingDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
 tblTestData <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
 tblTestDataX <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
 tblTestDataY <- read.table("E://data/Dropbox/private/working/data science/getting and cleaning data/CleanDataPrj1/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
@@ -15,3 +15,14 @@ tblFeatures <- read.table("E://data/Dropbox/private/working/data science/getting
 ## rename column names
 names(tblTrainingData) <- "ID"
 names(tblTestData) <- "ID"
+names(tblTrainingDataX) <- tblFeatures$V2
+names(tblTestDataX) <- tblFeatures$V2
+names(tblTrainingDataY) <- "Activity"
+names(tblTestDataY) <- "Activity"
+
+
+## merge both training and test data together
+tblTrainingDataXY <- cbind(tblTrainingData, tblTrainingDataX, tblTrainingDataY)
+tblTestDataXY <- cbind(tblTestData, tblTestDataX, tblTestDataY)
+tblData <- rbind(tblTrainingDataXY, tblTestDataXY)
+
